4bc309f1dfaa10e78497fcda80147b087131a4fb
DESKTOP-DLOPF19\SSAFY <khjhi4360@naver.com>
2025-10-27 10:34:06 +0900

    init

--- backend/demo/src/test/java/com/jde/demo/DemoApplicationTests.java
+++ backend/demo/src/test/java/com/jde/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ssafy.mvc.mainserver;
+package com.jde.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MainServerApplicationTests {
+class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
