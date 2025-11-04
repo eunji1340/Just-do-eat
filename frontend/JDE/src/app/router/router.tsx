ce8ef0d649254ccbb2e5b3ea61267c4cffecf6ca
ijioo <easywoo980902@gmail.com>
2025-11-04 19:28:00 +0900

    fix: v7_startTransition이 타입 에러 해결
    - @ts-expect-error로 의도적으로 타입 체크 우회

--- frontend/JDE/src/app/router/router.tsx
+++ frontend/JDE/src/app/router/router.tsx
@@ -2,20 +2,27 @@
 // 목적: 앱 라우팅 정의 (단일 책임: 라우팅)
 // 교체 포인트: 라우터 전환 시 여기만 수정 (ex. Remix, TanStack Router)
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AppLayout from '../layout/AppLayout'
-import MainPage from '../../pages/main/MainPage'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "../layout/AppLayout";
+import MainPage from "../../pages/main/MainPage";
 
 const router = createBrowserRouter([
-  { 
-    path: '/',
-    element:
-    <AppLayout>
-      <MainPage /> 
-    </AppLayout>
+  {
+    path: "/",
+    element: (
+      <AppLayout>
+        <MainPage />
+      </AppLayout>
+    ),
   },
-])
+]);
 
 export default function AppRouter() {
-  return <RouterProvider router={router} />
+  return (
+    <RouterProvider
+      router={router}
+      // @ ts-expect-error - v7_startTransition은 v6.4+에서 지원되지만 타입 정의가 누락됨
+      future={{ v7_startTransition: true }}
+    />
+  );
 }
