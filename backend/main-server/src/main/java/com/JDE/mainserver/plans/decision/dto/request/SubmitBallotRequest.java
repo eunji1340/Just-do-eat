1378fd23198fbfc6171d294894304f7eb9616849
SSM <nomio4001@naver.com>
2025-11-09 12:30:00 +0900

    add .gitignore

--- backend/main-server/src/main/java/com/JDE/mainserver/plans/decision/dto/request/SubmitBallotRequest.java
+++
@@ -1,7 +0,0 @@
-package com.JDE.mainserver.plans.decision.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record SubmitBallotRequest(
-        @NotNull Long restaurantId
-) {}
