0f0c9384a92e793e8c55008cd817a63085a0a340
So8oro <voo870@gmail.com>
2025-11-10 13:45:48 +0900

    feature: 데이터 수집 및 DB 주입 코드 추가
    Former-commit-id: cc94476e700e684f3b711df24b63b25a53ad0638

---
+++ back/db_script/01_staging.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c4ba773cf9118ba7bc6f07012d6c0f72433dc3e2c0bfdf51e69b7e449321e7d
+size 1498
---
+++ back/db_script/02_merge.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3f53e474496bca9de0f6b1895182333eaa7d7f2544ce838582678dafd7b0b56
+size 11035
