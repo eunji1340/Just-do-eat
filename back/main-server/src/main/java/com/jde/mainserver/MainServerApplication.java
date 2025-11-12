adb417996fbd765566d651c69d102cc47cd354da
신승민 <nomio4001@naver.com>
2025-11-12 14:22:21 +0900

    fix :connect DB
    Former-commit-id: 244d2c0ecdb6864d67b63a37673a2f446f35c6b6

--- back/main-server/src/main/java/com/jde/mainserver/MainServerApplication.java
+++ back/main-server/src/main/java/com/jde/mainserver/MainServerApplication.java
@@ -2,14 +2,10 @@ package com.jde.mainserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.jde.mainserver")
-// @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "com.jde.mainserver") // 레포지토리 루트
+@EnableJpaAuditing
 public class MainServerApplication {
 
     public static void main(String[] args) {
