377cd1d6be0f5cfdf19e350036470e7c908ff3a2
신승민 <nomio4001@naver.com>
2025-11-12 09:47:10 +0900

    fix: connect swagger
    Former-commit-id: 8847aef4a07bd947bc098877f859fa6d76a1da63

--- back/main-server/src/main/java/com/jde/mainserver/global/config/SecurityConfig.java
+++ back/main-server/src/main/java/com/jde/mainserver/global/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(a -> a
-				.requestMatchers("/","/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html","/swagger-resources/**","/swagger-resources" "/main/**").permitAll()
+				.requestMatchers("/","/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html","/swagger-resources/**","/swagger-resources", "/main/**").permitAll()
 				.anyRequest().permitAll()
 			)
 			.csrf(AbstractHttpConfigurer::disable)
