f32a825bc75b68981dfd5481667737feabcd089f
신승민 <nomio4001@naver.com>
2025-11-17 10:23:59 +0900

    feat: plan detail
    Former-commit-id: e0d88cf1f3be29104626f33118bd227b1a3f4e96

--- back/main-server/src/main/java/com/jde/mainserver/plan/converter/PlanConverter.java
+++ back/main-server/src/main/java/com/jde/mainserver/plan/converter/PlanConverter.java
@@ -1,24 +1,33 @@
 package com.jde.mainserver.plan.converter;
 
-import com.jde.mainserver.member.entity.Member;
+import com.jde.mainserver.global.api.KakaoLocalService;
 import com.jde.mainserver.plan.entity.Plan;
-import com.jde.mainserver.plan.entity.PlanParticipant;
 import com.jde.mainserver.plan.web.dto.response.PlanDetailResponse;
 import com.jde.mainserver.plan.web.dto.response.PlanParticipantInfo;
 import com.jde.mainserver.room.entity.Room;
-import org.geolatte.geom.Point;
+import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class PlanConverter {
+
+    private final KakaoLocalService kakaoLocalService;
 
     public PlanDetailResponse toPlanDetailResponse(Room room, Plan plan, String planManager, List<PlanParticipantInfo> planParticipantList) {
 
         Point point = plan.getPlanGeom();
-        String planPlace = "갱남";
-        String startAt = "2025-11-20 18:00";
+        String planPlace = kakaoLocalService.getAddressName(point);
+
+        LocalDateTime planDate = plan.getStartsAt();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String startAt = planDate.format(formatter);
+
 
         return PlanDetailResponse.builder()
                 .roomId(room.getRoomId())
